9ff00a3e47f840d6b2ef4b851eacac8c0fbf7b10
Alexey <a.a.kozyarchuk@gmail.com>
2023-05-14 20:59:38 +0500

    Finished doing task_016

--- task_016/Program.cs
+++ task_016/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* 16. Напишите программу, которая принимает на вход два числа
+       и проверяет, является ли одно число квадратом другого.
+
+5, 25 -> да
+-4, 16 -> да
+25, 5 -> да
+8, 9 -> нет        */
+
+// РЕШЕНИЕ ВЗЯТО С ПЕРВОЙ ЗАДАЧИ https://github.com/AlexeyKozyarchuk/GeekBrains_Seminar/blob/main/task_001/Program.cs
+
+void isSecondNumberSquaredIsEqualToFirst()
+{
+System.Console.WriteLine("Напишите, пожалуйста, первое число: ");
+int numberFirst = Int32.Parse(Console.ReadLine());
+System.Console.WriteLine("Напишите, пожалуйста, второе число: ");
+int numberSecond = Int32.Parse(Console.ReadLine());
+int numberSecondSquared = numberSecond * numberSecond;
+
+System.Console.Write($"a = {numberFirst}, b = {numberSecond}");
+if (numberSecondSquared == numberFirst) System.Console.WriteLine($" -> да");
+else System.Console.WriteLine($" -> нет");
+}
+
+isSecondNumberSquaredIsEqualToFirst();
