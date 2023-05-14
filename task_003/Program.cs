4bcf475bdd02254b538ab846ec7469dacb3dfc7a
Alexey <a.a.kozyarchuk@gmail.com>
2023-05-14 14:19:31 +0500

    Finished doing task_003

--- task_003/Program.cs
+++ task_003/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* 3. Напишите программу, которая будет выдавать
+      название дня недели по заданному номеру.
+
+3 -> Среда
+5 -> Пятница    */
+
+System.Console.WriteLine("Введите, пожалуйста, день недели: ");
+string dayOfWeek = Console.ReadLine();
+System.Console.Write("\n" + dayOfWeek + " ");
+switch (dayOfWeek)
+{
+    case "1":
+        System.Console.Write("-> Понедельник");
+        break;
+    case "2":
+        System.Console.Write("-> Вторник");
+        break;
+    case "3":
+        System.Console.Write("-> Среда");
+        break;
+    case "4":
+        System.Console.Write("-> Четверг");
+        break;
+    case "5":
+        System.Console.Write("-> Пятница");
+        break;
+    case "6":
+        System.Console.Write("-> Суббота");
+        break;
+    case "7":
+        System.Console.Write("-> Воскресенье");
+        break;
+}
+
+		
+		
