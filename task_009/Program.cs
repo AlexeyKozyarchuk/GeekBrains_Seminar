5de7e2dc609114c003a9a164c7b13b9202b01c84
Alexey <a.a.kozyarchuk@gmail.com>
2023-05-14 17:15:52 +0500

    Finished doing task_009

--- task_009/Program.cs
+++ task_009/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* 9. Напишите программу, которая выводит случайное число
+      из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+78 -> 8
+12 -> 2
+85 -> 8       */
+
+void GetRandomValueFromSegment()
+{
+    string number = Convert.ToString(new Random().Next(10, 99));
+    char max = number[0];
+    if (number[1] > number[0]) max = number[1];
+    System.Console.WriteLine($"{number} -> {max}");
+}
+
+GetRandomValueFromSegment();
