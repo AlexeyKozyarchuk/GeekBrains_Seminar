c55be81a77f63cff6c5b8141148e2562738c0b00
Alexey <a.a.kozyarchuk@gmail.com>
2023-05-14 18:33:00 +0500

    Finished doing task_011

--- task_011/Program.cs
+++ task_011/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿/* 11. Напишите программу, которая выводит случайное
+       трёхзначное число и удаляет вторую цифру этого числа.
+
+456 -> 46
+782 -> 72
+918 -> 98     */
+
+
+void GetRandomValueFromSegment()
+{
+    string numberEntered = Convert.ToString(new Random().Next(100, 999));
+    System.Console.WriteLine($"{numberEntered} -> {numberEntered[0]}{numberEntered[2]}");
+}
+
+GetRandomValueFromSegment();
