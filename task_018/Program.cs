4573133f67150e3f57d3cf550d20b0b17dc8d551
Alexey <a.a.kozyarchuk@gmail.com>
2023-05-14 23:16:37 +0500

    Finished doing task_018

--- task_018/Program.cs
+++ task_018/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 18: Напишите программу, которая по заданному номеру четверти,
+              показывает диапозон возмонжых координат точек в этой четверти (x и y).
+
+*/
+
+void GetNumberQuarter()
+{
+    System.Console.WriteLine("Введите номер четверти: ");
+    string numberQuarter = Console.ReadLine();
+    string rangeXAndY = "Диапозон возможных координат точне в этой четверти равен: ";
+    switch (numberQuarter)
+    {
+        case "1":
+            System.Console.WriteLine($"{rangeXAndY}(x = +бесконечность, y = +бесконечность)");
+            break;
+        case "2":
+            System.Console.WriteLine($"{rangeXAndY}(x = -бесконечность, y = +бесконечность)");
+            break;
+        case "3":
+            System.Console.WriteLine($"{rangeXAndY}(x = -бесконечность, y = -бесконечность)");
+            break;    
+        case "4":
+            System.Console.WriteLine($"{rangeXAndY}(x = +бесконечность, y = -бесконечность)");
+            break;
+        default:
+            System.Console.WriteLine("Вы ввели неправильное значение!");
+            break;     
+    }
+}
+
+GetNumberQuarter();
