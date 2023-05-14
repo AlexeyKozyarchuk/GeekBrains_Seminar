c431a80b450bdbead7febedce633e8b8e77bbd8f
Alexey <a.a.kozyarchuk@gmail.com>
2023-05-14 14:54:41 +0500

    Finished doing task_007

--- task_007/Program.cs
+++ task_007/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* 7. Напишите программу, которая принимает на вход
+      трёхзначное число и на выходе показывает последнюю цифру этого числа.
+
+456 -> 6
+782 -> 2
+918 -> 8      */
+
+void GetLastDigitOfNumber()
+{
+    System.Console.WriteLine("Введите число: ");
+    string number = Console.ReadLine();
+    int length = number.Length;
+    char result = number[length - 1];
+    System.Console.WriteLine($"{number} -> {result}");
+}
+
+GetLastDigitOfNumber();
