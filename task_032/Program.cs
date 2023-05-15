be123a81707bdbe7018ca6323b9f7a453d0e1ad3
Alexey <a.a.kozyarchuk@gmail.com>
2023-05-15 15:10:19 +0500

    Finishing doing task_032

---
+++ task_032/Program.cs
@@ -0,0 +1,51 @@
+﻿/*          Задача 32: Напишите программу замены элементов
+            массива: положительные элементы замените на
+            соответствующие отрицательные, и наоборот.
+
+[-4, -8, 8, 2] -> [4, 8, -8, -2]         */
+
+int[] FillArray()
+{
+    int size = 4;
+    int[] array = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(-9, 9);
+    }
+    return array;
+}
+
+void PrintArray(int[] collection)
+{
+    int length = collection.Length;
+    System.Console.Write("[");
+
+    for (int i = 0; i < length; i++)
+    {
+        if (i == length - 1)
+        {
+            System.Console.Write($"{collection[i]}]");
+            break;
+        }
+        System.Console.Write($"{collection[i]},");
+    }
+}
+
+int[] ConverterArray(int[] collection)
+{
+    int length = collection.Length;
+    int[] converterArray = new int[length];
+
+    for (int i = 0; i < length; i++)
+    {
+        converterArray[i] = collection[i] * -1;
+    }
+    return converterArray;
+}
+
+int[] array = FillArray();
+PrintArray(array);
+System.Console.Write(" -> ");
+int[] arrayConverter = ConverterArray(array);
+PrintArray(arrayConverter);
