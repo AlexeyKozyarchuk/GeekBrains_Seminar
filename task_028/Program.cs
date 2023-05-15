997deb5d282946b8861e8bde1d63fa9cbbe90ac0
Alexey <a.a.kozyarchuk@gmail.com>
2023-05-15 09:15:19 +0500

    Create task_028

---
+++ task_028/Program.cs
@@ -0,0 +1,22 @@
+﻿/*      Задача 28: Напишите программу, которая
+        принимает на вход число N и выдаёт
+        произведение чисел от 1 до N.
+
+4 -> 24
+5 -> 120     */
+
+void GetProductOfNumbers()
+{
+    System.Console.WriteLine("\nВведите, пожалуйста, число: ");
+    int number = int.Parse(Console.ReadLine());
+    System.Console.WriteLine();
+    int composition = 0;
+
+    for (int i = 1; i <= number; i++)
+    {
+        composition *= i;
+    }
+    System.Console.WriteLine($"{number} -> {composition}");
+}
+
+GetProductOfNumbers();
