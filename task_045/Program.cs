d00ff9c64c917fbe97bc5b0b8a861ec6a6947a25
Alexey <a.a.kozyarchuk@gmail.com>
2023-05-16 14:39:23 +0500

    Finished doing task_045

--- task_045/Program.cs
+++ task_045/Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*          Задача 45: Напишите программу, которая будет создавать
+            копию заданного массива с помощью поэлементного
+            копирования. 
+
+*/
+
+int[] FillArray()
+{
+    Random rnd = new Random();
+    int size = rnd.Next(3, 10);
+    int[] array = new int[size];
+    int length = array.Length;
+
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = rnd.Next(1, 10);
+    }
+    return array;
+}
+
+void PrintArray(int[] collection)
+{
+    int length = collection.Length;
+    System.Console.Write("[");
+
+    for (int i = 0; i < length; i++)
+    {
+        if (i == length - 1)
+        {
+            System.Console.Write($"{collection[i]}]");
+            break;
+        }
+        System.Console.Write($"{collection[i]},");
+    }
+}
+
+int[] CopyArray(int[] collection)
+{
+    int length = collection.Length;
+    int[] arrayCopy = new int[length];
+
+    for (int i = 0; i < length; i++)
+    {
+        arrayCopy[i] = collection[i];
+    }
+    return arrayCopy;
+}
+
+int[] array = FillArray();
+PrintArray(array);
+System.Console.Write(" -> копия - ");
+int[] arrayCopy = CopyArray(array);
+PrintArray(arrayCopy);
